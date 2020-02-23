15828cb778765644b58e8247cfe2860bf4c17f7f
Anatoliy Udarczev <toly.udarczev@yandex.ru>
2020-02-23 15:46:03 +0300

    Add task4.27

---
+++ option27/task4/data.py
@@ -0,0 +1,127 @@
+import datetime
+import json
+import re
+
+
+class Data(object):
+    _all_lang = {
+        'ru': 'ru-lang.json',
+        'en': 'en-lang.json'
+    }
+
+    _templates = {}
+
+    def __init__(self, lang='ru', time=datetime.datetime.now()):
+        with open(Data._all_lang[lang], 'r', encoding='utf-8') as file_read:
+            self._lang = json.loads(file_read.read())
+        self.time = time
+
+    def format(self, template: str) -> str:
+        templates = Data._templates
+        result = template
+        for key in templates.keys():
+            groups = re.findall(key, template)
+            if groups:
+                for group in groups:
+                    result = result.replace(group, templates[key](g=group, la=self._lang, t=self.time))
+        return result
+
+    @staticmethod
+    def add_template(temp: str, f):
+        Data._templates[temp] = f
+
+
+def same_low_up_case(origin: str, same: str):
+    if origin.islower():
+        return same.lower()
+    elif origin.isupper():
+        return same.upper()
+    else:
+        return same[0].upper() + same[1:]
+
+
+def year(**kwargs):
+    y = str(kwargs['t'].year)
+    size = len(kwargs['g'])
+    if size < len(y):
+        return y[size:]
+    else:
+        return y.zfill(size)
+
+
+def _mm(**kwargs):
+    m = str(kwargs['t'].month)
+    return m.zfill(2)
+
+
+def _mon(**kwargs):
+    i = kwargs['t'].month
+    m = kwargs['la']['month']['short'][i]
+    return same_low_up_case(kwargs['g'], m)
+
+
+def _month(**kwargs):
+    i = kwargs['t'].month
+    m = kwargs['la']['month']['long'][i]
+    return same_low_up_case(kwargs['g'], m)
+
+
+def _day(**kwargs):
+    i = kwargs['t'].weekday()
+    d = kwargs['la']['day']['long'][i]
+    return same_low_up_case(kwargs['g'], d)
+
+
+def _dy(**kwargs):
+    i = kwargs['t'].weekday()
+    d = kwargs['la']['day']['short'][i]
+    return same_low_up_case(kwargs['g'], d)
+
+
+def _dd(**kwargs):
+    d = str(kwargs['t'].day)
+    return d.zfill(2)
+
+
+def _ddd(**kwargs):
+    d = str(kwargs['t'].timetuple().tm_yday)
+    return d.zfill(3)
+
+
+def _hour(**kwargs):
+    h = str(kwargs['t'].hour % 12)
+    return h.zfill(2)
+
+
+def _hour12(**kwargs):
+    h = kwargs['t'].hour
+    if h <= 12:
+        suffix = kwargs['h12']['am']
+    else:
+        suffix = kwargs['h12']['pm']
+    h = str(h % 12)
+    return h.zfill(2) + suffix
+
+
+def _hour24(**kwargs):
+    h = str(kwargs['t'].hour)
+    return h.zfill(2)
+
+
+_functions = {
+    'Y[Y]+': year,
+    'Year': year,
+    'DD': _dd,
+    'DDD': _ddd,
+    '[Dd][Yy]': _dy,
+    'Day|DAY|day': _day,
+    'MM': _mm,
+    'Month|MONTH|month': _mon,
+    'Mon|MON|mon': _month,
+    'HH': _hour,
+    'HH12': _hour12,
+    'HH24': _hour24
+}
+
+for item in _functions.items():
+    Data.add_template(item[0], item[1])
---
+++ option27/task4/main.py
@@ -0,0 +1,7 @@
+from option27.task4.data import Data
+
+data = Data('ru')
+template = input("Input format date: ")
+print(data.format(template))
+
+
