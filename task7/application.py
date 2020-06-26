8a5e088718d8c352b634998e4146c89c85291312
Anatoliy Udarczev <toly.udarczev@yandex.ru>
2020-06-26 16:28:47 +0300

    Add task7

---
+++ task7/application.py
@@ -0,0 +1,41 @@
+import re
+
+from flask import Flask, redirect, url_for, render_template, request
+import urllib.request
+
+app = Flask(__name__)
+
+
+@app.route('/index', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/index', methods=['POST'])
+def search():
+    url = request.form['url']
+    return redirect(url_for(endpoint='scan', args=url))
+
+
+@app.route('/scanner', methods=['GET'])
+def scan():
+    response = urllib.request.urlopen(request.args['args'])
+    html = response.read().decode('utf-8')
+    result = {}
+    splited = re.split(r'<[^>]*>', html)
+    for part in splited:
+        part = re.split(r'[\->@%_\r\t\n,!?.:; (){}\\|/=\[\]\"\'#&0-9]', part)
+        for s in part:
+            if s == '':
+                continue
+            if s not in result:
+                result[s] = 1
+            else:
+                result[s] = result[s] + 1
+
+    def sort(item):
+        return item[1]
+    return render_template('result.html', words=sorted(result.items(), key=sort, reverse=True))
+
+
+app.run()
